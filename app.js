1b316cd5196993ff666d144bc0b4ef96e23db7e2
17aim17 <17aim17@gmail.com>
2019-04-21 03:53:00 +0530

    changed mongo_uri

--- app.js
+++ app.js
@@ -39,14 +39,14 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-var url = process.env.DATABASEURL;
+var url = process.env.MONGO_URI;
 mongoose.connect(url, { useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 // to check if user is signed in or not and updating the navbar instaedd of adding currentUser in allroures as /campgrounds we use middeware this
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
   res.locals.error = req.flash('error');
   res.locals.success = req.flash('success');
@@ -57,6 +57,6 @@ app.use(indexRoutes);
 app.use(campgroundsRoutes);
 app.use(commentRoutes);
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Server started at ' + port);
 });
